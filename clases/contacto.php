b82be524b6b4b11ea0942f5c1d9390f6f686fcd8
ferbj <ferbj200@gmail.com>
2016-10-24 00:11:43 -0500

    Update contacto.php

--- clases/contacto.php
+++ clases/contacto.php
@@ -5,31 +5,31 @@ include_once('conexion.php');
 class Contacto
 {
 
- private $personaId;
- private $nombre;
- private $apellido;
- private $direccion;
- private $telefono;
- private $email; 
- private $sexo;
-     
- 
- function __construct($personaId=NULL,$nombre=NULL,$apellido=NULL,$telefono=NULL,$direccion=NULL,$email=NULL,$sexo=NULL)
-	 {
-	 	$nuevaConexion=new Conexion();
-	 	$conexion=$nuevaConexion->getConexion();
+	private $personaId;
+	private $nombre;
+	private $apellido;
+	private $direccion;
+	private $telefono;
+	private $email; 
+	private $sexo;
+
+
+	function __construct($personaId=NULL,$nombre=NULL,$apellido=NULL,$telefono=NULL,$direccion=NULL,$email=NULL,$sexo=NULL)
+	{
+		$nuevaConexion=new Conexion();
+		$conexion=$nuevaConexion->getConexion();
 		 //seteo los atributos
-		 $this->personaId = $conexion->real_escape_string($personaId);
-		 $this->nombre = $conexion->real_escape_string($nombre);
-		 $this->apellido = $conexion->real_escape_string($apellido);
-		 $this->telefono = $conexion->real_escape_string($telefono);
-		 $this->direccion = $conexion->real_escape_string($direccion);
-	     $this->email=$conexion->real_escape_string($email);	 
-         $this->sexo = $conexion->real_escape_string($sexo);
-	
-	
-	 }
-	 
+		$this->personaId = $conexion->real_escape_string($personaId);
+		$this->nombre = $conexion->real_escape_string($nombre);
+		$this->apellido = $conexion->real_escape_string($apellido);
+		$this->telefono = $conexion->real_escape_string($telefono);
+		$this->direccion = $conexion->real_escape_string($direccion);
+		$this->email=$conexion->real_escape_string($email);	 
+		$this->sexo = $conexion->real_escape_string($sexo);
+
+
+	}
+
 	public function agregar()
 	{
 		
@@ -41,7 +41,9 @@ class Contacto
 		*/
 		$sentencia="INSERT INTO contactos(personaId,nombre,apellido,direccion,telefono,email,sexo) VALUES (NULL,?,?,?,?,?,?)";
 		$stmt= $conexion->prepare($sentencia);
-		$stmt->bind_param('issssss',$this->nombre,$this->apellido,$this->direccion,$this->telefono,$this->email,$this->sexo);
+		
+		$stmt->bind_param('ssssss',$this->nombre,$this->apellido,$this->direccion,$this->telefono,$this->email,$this->sexo);
+		//var_dump($stmt);
 		//$stmt->execute();
 		//if ($conexion->query($sentencia)) {
 		
@@ -50,20 +52,22 @@ class Contacto
 		}
 		else
 		{
-			return $sentencia."<br>"."Error al ejecutar la sentencia".$conexion->errno." :".$conexion->error;
+			return $sentencia."<br>"."Error al ejecutar la sentencia".$conexion->errno." :".$conexion->error." ".$stmt->error;
 		}
 		
 	}
-				 
-		 public function editar()
-		{
+
+	public function editar()
+	{
 		
 		$nuevaConexion=new Conexion();
 		$conexion=$nuevaConexion->getConexion();
 
 		//$sentencia="UPDATE contactos SET nombre = ' $this->nombre', apellido = '$this->apellido', telefono = '$this->telefono', direccion = '$this->direccion',email= '$this->email', sexo = '$this->sexo' WHERE personaId = '$this->personaId'";
 		$sentencia="UPDATE contactos SET nombre = ?, apellido=?, telefono=?,direccion=?,email=?,sexo=? WHERE personaId=?";
-		$stmt->$conexion->prepare($sentencia);
+		
+		$stmt = $conexion->prepare($sentencia);
+		
 		$stmt->bind_param('ssssssi',$this->nombre,$this->apellido,$this->telefono,$this->direccion,$this->email,$this->sexo,$this->personaId);
 		//if ($conexion->query($sentencia)) {
 		
@@ -72,7 +76,7 @@ class Contacto
 		}
 		else
 		{
-			return $sentencia."<br>"."Error al ejecutar la sentencia".$conexion->errno." :".$conexion->error;
+			return $sentencia."<br>"."Error al ejecutar la sentencia".$conexion->errno." :".$conexion->error." ".$stmt->error;
 		}
 	}
 
@@ -83,7 +87,7 @@ class Contacto
 		
 		//$sentencia="DELETE FROM contactos WHERE personaId=".$this->personaId;
 		$sentencia ="DELETE FROM contactos WHERE personaId=?";
-		$stmt->$conexion->prepare($sentencia);
+		$stmt=$conexion->prepare($sentencia);
 		$stmt->bind_param('i',$this->personaId);
 		
 		//if ($conexion->query($sentencia)) {
@@ -95,10 +99,10 @@ class Contacto
 			return $sentencia."<br>"."Error al ejecutar la sentencia".$conexion->errno." :".$conexion->error;
 		}
 		
-	
-}
 
-public function buscar()
+	}
+
+	public function buscar()
 	{
 		$nuevaConexion=new Conexion();
 		$conexion=$nuevaConexion->getConexion();
@@ -107,50 +111,50 @@ public function buscar()
 		if($this->nombre!=NULL ||  $this->apellido!=NULL || $this->telefono!=NULL || $this->direccion!=NULL || $this->email!=NULL ||$this->sexo!=NULL )
 		{
 			$sentencia.=" WHERE ";
-		
-		
-		if($this->nombre!=NULL)
-		{
-			$sentencia.=" nombre LIKE '%$this->nombre%' && ";
-		}
-		
-		if($this->apellido!=NULL)
-		{
-			$sentencia.=" apellido LIKE '%$this->apellido%' && ";
-		}
-		
-				
-		if($this->direccion!=NULL)
-		{
-			$sentencia.=" direccion LIKE '%$this->direccion%' && ";
-		}
-		
-        if($this->telefono!=NULL)
-		{
-			$sentencia.=" telefono LIKE '%$this->telefono%' && ";
-		}
-		if($this->email!=NULL)
-		{
-			$sentencia.=" email LIKE '%$this->email%' && ";
-		}
-		            
-        if($this->sexo!=NULL)
-		{
-			$sentencia.=" sexo LIKE '%$this->sexo%' && ";
-		}
-		
-		$sentencia=substr($sentencia,0,strlen($sentencia)-4);	
+
+
+			if($this->nombre!=NULL)
+			{
+				$sentencia.=" nombre LIKE '%$this->nombre%' && ";
+			}
+
+			if($this->apellido!=NULL)
+			{
+				$sentencia.=" apellido LIKE '%$this->apellido%' && ";
+			}
+
+
+			if($this->direccion!=NULL)
+			{
+				$sentencia.=" direccion LIKE '%$this->direccion%' && ";
+			}
+
+			if($this->telefono!=NULL)
+			{
+				$sentencia.=" telefono LIKE '%$this->telefono%' && ";
+			}
+			if($this->email!=NULL)
+			{
+				$sentencia.=" email LIKE '%$this->email%' && ";
+			}
+
+			if($this->sexo!=NULL)
+			{
+				$sentencia.=" sexo LIKE '%$this->sexo%' && ";
+			}
+
+			$sentencia=substr($sentencia,0,strlen($sentencia)-4);	
 		//echo $sentencia;
 		}
 		
 		$sentencia.="  ORDER BY apellido,nombre";	
-	
-			
-		return $conexion->query($sentencia);
-			
-}
 
-  public function getContacto()
+
+		return $conexion->query($sentencia);
+
+	}
+
+	public function getContacto()
 	{
 		$nuevaConexion=new Conexion();
 		$conexion=$nuevaConexion->getConexion();
@@ -163,42 +167,42 @@ public function buscar()
 		$this->telefono=$elemento->telefono;
 		$this->direccion=$elemento->direccion;
 		$this->email=$elemento->email;
-        $this->sexo=$elemento->sexo;
+		$this->sexo=$elemento->sexo;
 		return $this;
-			
-    }
-    
-    public function getNombre()
-    {
+
+	}
+
+	public function getNombre()
+	{
 		return $this->nombre;
 	}
 	
 	public function getApellido()
-    {
+	{
 		return $this->apellido;
 	}
 	
 	public function getTelefono()
-    {
+	{
 		return $this->telefono;
 	}
 	
 	public function getDireccion()
-    {
+	{
 		return $this->direccion;
 	}
 	
-    public function getEmail()
-    {
-        return $this->email;    
-    }
+	public function getEmail()
+	{
+		return $this->email;    
+	}
 	
-    public function getSexo()
-    {
+	public function getSexo()
+	{
 		return $this->sexo;
 	}
-    
- 
+
+
 
 }
 ?>
